13cf347f45ac45403e556dbe2e5de4b670486a4c
suraj ghimire <sgh@surajs-MacBook-Air.local>
2021-07-27 12:08:54 +0545

    initial-commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 0.1.0
+    </div>
+    <strong>Copyright &copy; <a href="https://www.milaptech.com.np/" target = "_blank">Milap Technologies</a>-2021.</strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
